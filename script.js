ea9bb9d4f470d8e3e054a240365d0b2b4669ae42
Richard Robinson Campos <riqui19@gmail.com>
2020-01-24 16:21:08 -0200

    Finalização menu dropdown site

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ $('.carrousel').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
 });
+
+//Mostra e fecha menu mobile ao clicar no botão
+function showMenuFunction() {
+    document.getElementById("menu-mobile-content").classList.toggle("show");
+}
+
+/*
+//Fecha o menu se clicar fora dele
+window.onclick = function(event) {
+    if (!event.target.matches('.check')) {
+        var dropdowns = document.getElementsByClassName("menu-mobile-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+*/
